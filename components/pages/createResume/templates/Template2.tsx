973348bb4beb598d33f1c65442ac849f02e07f47
manoj <manojthulung03@gmail.com>
2024-04-20 00:17:45 +0545

    added templalte2 resume

--- components/pages/createResume/templates/Template2.tsx
+++ components/pages/createResume/templates/Template2.tsx
@@ -19,7 +19,7 @@ const Template2 = ({ margin }: Template2Props) => {
         <hr style={{ marginTop: margin }} />
       </div>
       {/* PROFILE & CAREER OBJECTIVE ////////////////////////////////////// */}
-      <div className="t2_sec" style={{ marginTop: -margin }}>
+      <div className="t2_sec !pt-2" style={{ marginTop: -margin }}>
         <h2 className="t2_title">PROFILE & CAREER OBJECTIVE</h2>
         <p className="px-1 pt-[6px] t2_text">
           With 1+ years of experience in developing user-friendly, responsive
@@ -33,6 +33,18 @@ const Template2 = ({ margin }: Template2Props) => {
           skills, allowing me to create user-friendly front-end solutions and
           shape the web.
         </p>
+      </div>
+
+      {/* SOCIAL LINKS ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title">CONTACT</h2>
+        <div className="pt-2 px-1">
+          <p className="t2_link">https://www.linkedin.com/in/manojrai1/</p>
+          <p className="t2_link">https://www.linkedin.com/in/manojrai1/</p>
+          <p className="t2_link">https://www.linkedin.com/in/manojrai1/</p>
+          <p className="t2_link">https://www.linkedin.com/in/manojrai1/</p>
+        </div>
       </div>
 
       {/* EXPERIENCE ///////////////////////////////////////////// */}
@@ -78,6 +90,165 @@ const Template2 = ({ margin }: Template2Props) => {
               solutions.
             </li>
           </ul>
+        </div>
+      </div>
+
+      {/* EDUCATION ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title">EDUCATION</h2>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">BSc (Hons) Computing</h2>
+          <div className="flex items-center justify-between">
+            <p className="t2_sub-title">
+              Itahari International College | Morang, Nepal
+            </p>
+            <p className="t2_sub-title italic">2022 - Present</p>
+          </div>
+        </div>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">BSc (Hons) Computing</h2>
+          <div className="flex items-center justify-between">
+            <p className="t2_sub-title">
+              Itahari International College | Morang, Nepal
+            </p>
+            <p className="t2_sub-title italic">2022 - Present</p>
+          </div>
+        </div>
+      </div>
+
+      {/* SKILLS ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title uppercase">Technical SKills</h2>
+        <div className="pt-2 px-1 flex items-center justify-start gap-x-2">
+          <p className="t2_text">Skill 1</p>|<p className="t2_text">Skill 1</p>|
+          <p className="t2_text">Skill 1</p>|<p className="t2_text">Skill 1</p>|
+          <p className="t2_text">Skill 1</p>|<p className="t2_text">Skill 1</p>|
+          <p className="t2_text">Skill 1</p>
+        </div>
+      </div>
+
+      {/* PROJECTS ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title uppercase">PROJECTS</h2>
+        <div className="pt-2 px-1">
+          <p className="t2_sub-title">Code Play Club</p>
+          <p className="t2_text">
+            A DGBL (Digital Game-Based Learning Platform) web platform to
+            provide knowledge on CSS Selector and JavaScript DOM in an
+            interactive way through gameplay. In the game, a user has to write
+            the correct code to complete the task and move to the next level.
+          </p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <p className="t2_sub-title">Code Play Club</p>
+          <p className="t2_text">
+            A DGBL (Digital Game-Based Learning Platform) web platform to
+            provide knowledge on CSS Selector and JavaScript DOM in an
+            interactive way through gameplay. In the game, a user has to write
+            the correct code to complete the task and move to the next level.
+          </p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <p className="t2_sub-title">Code Play Club</p>
+          <p className="t2_text">
+            A DGBL (Digital Game-Based Learning Platform) web platform to
+            provide knowledge on CSS Selector and JavaScript DOM in an
+            interactive way through gameplay. In the game, a user has to write
+            the correct code to complete the task and move to the next level.
+          </p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <p className="t2_sub-title">Code Play Club</p>
+          <p className="t2_text">
+            A DGBL (Digital Game-Based Learning Platform) web platform to
+            provide knowledge on CSS Selector and JavaScript DOM in an
+            interactive way through gameplay. In the game, a user has to write
+            the correct code to complete the task and move to the next level.
+          </p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+      </div>
+
+      {/* CERTIFICATION ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title">CERTIFICATION</h2>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">Certificate Title</h2>
+          <div className="flex items-center justify-between">
+            <p className="t2_sub-title">Itahari International College</p>
+            <p className="t2_sub-title italic">2022 January</p>
+          </div>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">Certificate Title</h2>
+          <div className="flex items-center justify-between">
+            <p className="t2_sub-title">Itahari International College</p>
+            <p className="t2_sub-title italic">2022 January</p>
+          </div>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">Certificate Title</h2>
+          <div className="flex items-center justify-between">
+            <p className="t2_sub-title">Itahari International College</p>
+            <p className="t2_sub-title italic">2022 January</p>
+          </div>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+      </div>
+
+      {/* LANGUAGES ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title">LANGUAGES</h2>
+        <div className="pt-2 px-1 flex items-center justify-start gap-x-2">
+          <p className="t2_text">Language 1</p>|
+          <p className="t2_text">Nepali 1</p>|
+          <p className="t2_text">Chinese 1</p>|
+        </div>
+      </div>
+
+      {/* REFERENCES ///////////////////////////////////////////// */}
+      <hr style={{ marginTop: margin }} />
+      <div className="t2_sec" style={{ marginTop: -margin }}>
+        <h2 className="t2_title">REFERENCES</h2>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">McKenzie Odom - Velit dolore ex sint</h2>
+          <p className="t2_sub-title">Bridges and Dale Co</p>
+          <p className="t2_text italic">nixadiv@mailinator.com</p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
+        </div>
+        <div className="pt-2 px-1">
+          <h2 className="font-bold">McKenzie Odom - Velit dolore ex sint</h2>
+          <p className="t2_sub-title">Bridges and Dale Co</p>
+          <p className="t2_text italic">nixadiv@mailinator.com</p>
+          <p className="t2_link">
+            https://manojrai.info.np/manoj-rai-resume.pdf
+          </p>
         </div>
       </div>
     </>
