7e19127c232184d9829e05fe209f7abe72b86a3d
manoj <manojthulung03@gmail.com>
2024-05-01 23:24:15 +0545

    added redux persist

--- redux/Providers.tsx
+++ redux/Providers.tsx
@@ -3,13 +3,21 @@
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
+const persistor = persistStore(store);
+
 const Providers = ({ children }: ProvidersProps) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  );
 };
 
 export default Providers;
--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,9 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import resumeReducer from "../slice/resumeSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+// import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  resume: resumeReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    resume: resumeReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
   },
 });
 
